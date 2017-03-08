c123fe6579ec2b8f48dc0f42cfbb2524adefb4ff
Dion Hiananto <sdgs72@hotmail.com>
2017-03-07 19:52:29 -0600

    hello world

---
+++ .worksheet/src/scala_test.scala
@@ -0,0 +1,14 @@
+object scala_test {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(63); 
+  println("Welcome to the Scala worksheet")}
+}
+
+ object HelloWorld {
+    def main(args: Array[String]) {
+      println("Hello, world! " + args.toList)
+    }
+ }
+ 
+HelloWorld.main("ref");
+ 
+  
+  
